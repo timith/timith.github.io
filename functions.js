220c92d435d4c37f333b429c17e1a251ab675421
timith <42706837+timith@users.noreply.github.com>
2019-11-09 13:35:04 -0700

    Added music tab
    added music tab and few songs with audio

--- functions.js
+++ functions.js
@@ -2,10 +2,15 @@ function password(){
 	var pword = prompt("enter the webcode:");
 	var pdfpass = 3.1415;
 	var gamepass = 2.7182;
+	var musicpass = "music";
 	if (pword==pdfpass) {
 		document.getElementById("link1").innerHTML = "PDFS";
 	}
 	if(pword == gamepass){
 		document.getElementById("link2").innerHTML = "Games";
+	}
+
+	if(pword == musicpass){
+		document.getElementById("link3").innerHTML = "Music";
 	}
 }
